353b09b06c0d718ae9aca18f3538309bbbec7cc3
Abduhamid <Abdukhamid000@gmail.com>
2024-03-01 10:11:37 +0500

    fix: tag

--- src/components/Cards/StudentCourseCard.vue
+++ src/components/Cards/StudentCourseCard.vue
@@ -4,7 +4,7 @@
 
     <div class="flex items-center justify-between mt-6">
       <div class="flex items-center gap-2">
-     <img src="/svg/uranus.svg" alt=""/>
+        <img src="/svg/uranus.svg" alt="" />
         <p class="text-black !leading-[23px] text-base font-medium">
           Uranus Invest
         </p>
@@ -19,48 +19,109 @@
 
     <div class="mt-8 flex items-center gap-4">
       <div class="flex items-center gap-2">
-        <p class=text-sm>Трейдинг</p>
+        <p class="text-sm">Трейдинг</p>
         <div>|</div>
       </div>
       <div class="flex items-center gap-6">
         <div class="flex items-center gap-2">
-          <svg width="24.000000" height="24.000000" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <desc>
-                Created with Pixso.
-            </desc>
+          <svg
+            width="24.000000"
+            height="24.000000"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <desc>Created with Pixso.</desc>
             <defs>
               <clipPath id="clip4_790">
-                <rect id="play-circle" width="24.000000" height="24.000000" fill="white" fill-opacity="0"/>
+                <rect
+                  id="play-circle"
+                  width="24.000000"
+                  height="24.000000"
+                  fill="white"
+                  fill-opacity="0"
+                />
               </clipPath>
             </defs>
             <g clip-path="url(#clip4_790)">
-              <circle id="Ellipse 1909" cx="12.000000" cy="12.000000" r="10.000000" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000"/>
-              <path id="Vector 3642" d="M15.4531 12.3948C15.5156 12.136 15.5156 11.864 15.4531 11.6052C15.3018 10.9785 14.5859 10.5356 13.1538 9.6499C11.7695 8.79346 11.0776 8.36548 10.52 8.5376C10.2896 8.60864 10.0791 8.7439 9.90967 8.92993C9.5 9.38013 9.5 10.2534 9.5 12C9.5 13.7466 9.5 14.6199 9.90967 15.0701C10.0791 15.2561 10.2896 15.3914 10.52 15.4624C11.0776 15.6345 11.7695 15.2065 13.1538 14.3501C14.5859 13.4644 15.3018 13.0215 15.4531 12.3948Z" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000" stroke-linejoin="round"/>
+              <circle
+                id="Ellipse 1909"
+                cx="12.000000"
+                cy="12.000000"
+                r="10.000000"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+              />
+              <path
+                id="Vector 3642"
+                d="M15.4531 12.3948C15.5156 12.136 15.5156 11.864 15.4531 11.6052C15.3018 10.9785 14.5859 10.5356 13.1538 9.6499C11.7695 8.79346 11.0776 8.36548 10.52 8.5376C10.2896 8.60864 10.0791 8.7439 9.90967 8.92993C9.5 9.38013 9.5 10.2534 9.5 12C9.5 13.7466 9.5 14.6199 9.90967 15.0701C10.0791 15.2561 10.2896 15.3914 10.52 15.4624C11.0776 15.6345 11.7695 15.2065 13.1538 14.3501C14.5859 13.4644 15.3018 13.0215 15.4531 12.3948Z"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+                stroke-linejoin="round"
+              />
             </g>
           </svg>
-<span class="text-sm">60 видео</span>
+          <span class="text-sm">60 видео</span>
         </div>
-  <div class="flex items-center gap-2">
+        <div class="flex items-center gap-2">
+          <svg
+            width="24.000000"
+            height="24.000000"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <desc>Created with Pixso.</desc>
+            <defs>
+              <clipPath id="clip4_798">
+                <rect
+                  id="user-group"
+                  width="24.000000"
+                  height="24.000000"
+                  fill="white"
+                  fill-opacity="0"
+                />
+              </clipPath>
+            </defs>
+            <g clip-path="url(#clip4_798)">
+              <path
+                id="Vector"
+                d="M19 13C19.7896 13.0715 20.5679 13.3757 21.2651 13.9126C21.9512 14.4409 23.75 15.5195 22.6543 16.8691C22.1191 17.5286 21.5234 18 20.7739 18M18 6C19.3809 6 20.5 7.11938 20.5 8.5C20.5 9.88062 19.3809 11 18 11"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+              />
+              <path
+                id="Vector"
+                d="M5 13C4.21045 13.0715 3.43213 13.3757 2.73486 13.9126C2.04883 14.4409 0.25 15.5195 1.3457 16.8691C1.88086 17.5286 2.47656 18 3.22607 18M5.5 6C4.11914 6 3 7.11938 3 8.5C3 9.88062 4.11914 11 5.5 11"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+              />
+              <path
+                id="Vector"
+                d="M15.916 15.1111C16.938 15.7429 19.6172 17.0332 17.9854 18.6475C17.188 19.436 16.3003 20 15.1846 20L8.81543 20C7.69971 20 6.81201 19.436 6.01465 18.6475C4.38281 17.0332 7.06201 15.7429 8.08398 15.1111C10.48 13.6296 13.52 13.6296 15.916 15.1111Z"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+                stroke-linejoin="round"
+              />
+              <path
+                id="Ellipse 1381"
+                d="M12 11C10.0669 11 8.5 9.43311 8.5 7.5C8.5 5.56689 10.0669 4 12 4C13.9331 4 15.5 5.56689 15.5 7.5C15.5 9.43311 13.9331 11 12 11Z"
+                stroke="#575757"
+                stroke-opacity="1.000000"
+                stroke-width="1.500000"
+              />
+            </g>
+          </svg>
 
-    <svg width="24.000000" height="24.000000" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-	<desc>
-			Created with Pixso.
-	</desc>
-	<defs>
-		<clipPath id="clip4_798">
-			<rect id="user-group" width="24.000000" height="24.000000" fill="white" fill-opacity="0"/>
-		</clipPath>
-	</defs>
-	<g clip-path="url(#clip4_798)">
-		<path id="Vector" d="M19 13C19.7896 13.0715 20.5679 13.3757 21.2651 13.9126C21.9512 14.4409 23.75 15.5195 22.6543 16.8691C22.1191 17.5286 21.5234 18 20.7739 18M18 6C19.3809 6 20.5 7.11938 20.5 8.5C20.5 9.88062 19.3809 11 18 11" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000"/>
-		<path id="Vector" d="M5 13C4.21045 13.0715 3.43213 13.3757 2.73486 13.9126C2.04883 14.4409 0.25 15.5195 1.3457 16.8691C1.88086 17.5286 2.47656 18 3.22607 18M5.5 6C4.11914 6 3 7.11938 3 8.5C3 9.88062 4.11914 11 5.5 11" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000"/>
-		<path id="Vector" d="M15.916 15.1111C16.938 15.7429 19.6172 17.0332 17.9854 18.6475C17.188 19.436 16.3003 20 15.1846 20L8.81543 20C7.69971 20 6.81201 19.436 6.01465 18.6475C4.38281 17.0332 7.06201 15.7429 8.08398 15.1111C10.48 13.6296 13.52 13.6296 15.916 15.1111Z" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000" stroke-linejoin="round"/>
-		<path id="Ellipse 1381" d="M12 11C10.0669 11 8.5 9.43311 8.5 7.5C8.5 5.56689 10.0669 4 12 4C13.9331 4 15.5 5.56689 15.5 7.5C15.5 9.43311 13.9331 11 12 11Z" stroke="#575757" stroke-opacity="1.000000" stroke-width="1.500000"/>
-	</g>
-</svg>
-
-<span class="text-sm">500+ учащихся</span>
-  </div>
+          <span class="text-sm">500+ учащихся</span>
+        </div>
       </div>
     </div>
     <div class="mt-12 items-center flex justify-between">
@@ -126,4 +187,4 @@
   box-shadow: 0px 4px 14px 0px rgba(46, 5, 255, 0.08);
   background: rgb(253, 253, 255);
 }
-</style
+</style>

fb51f33e99bbe596df96d7d5c45cecf3f1036292
Abduhamid <Abdukhamid000@gmail.com>
2024-03-01 20:40:51 +0500

    fix: issues

--- src/pages/Student.vue
+++ src/pages/Student.vue
@@ -4,6 +4,7 @@
     <AboutMercury />
     <Courses />
     <Partners />
+    <Followers />
     <Testamonial />
     <Contact />
     <Faq />
@@ -15,6 +16,7 @@ import AboutMercury from '@/components/Sections/AboutMercury.vue'
 import Contact from '@/components/Sections/Contact.vue'
 import Courses from '@/components/Sections/Courses.vue'
 import Faq from '@/components/Sections/Faq.vue'
+import Followers from '@/components/Sections/Followers.vue'
 import Partners from '@/components/Sections/Partners.vue'
 import StudentHero from '@/components/Sections/StudentHero.vue'
 import Testamonial from '@/components/Sections/Testamonial.vue'

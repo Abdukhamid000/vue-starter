550f4dcd97d1736129427b572da0d71b20e13404
Abduhamid <Abdukhamid000@gmail.com>
2024-03-02 07:23:38 +0500

    fix: issues

--- src/components/Cards/SubscriptionCard.vue
+++ src/components/Cards/SubscriptionCard.vue
@@ -1,32 +1,34 @@
 <template>
   <div
-    class="bg-green-300 rounded-3xl px-8 pt-12 pb-16 max-md:pb-10 max-md:px-4"
+    class="bg-green-300 rounded-3xl px-8 pt-12 pb-16 max-lg:pb-10 max-lg:px-4 max-lg:pt-6 max-md:pb-10 max-md:px-4"
     :class="{ 'border-[1.5px] border-main': popular }"
   >
     <div class="max-w-[373px] w-full flex flex-col items-center">
-      <h5 class="text-2xl font-medium !leading-[35px] text-center">Вебинар</h5>
+      <h5 class="text-2xl font-medium !leading-[35px] text-center">
+        {{ title }}
+      </h5>
       <p class="mt-4 text-green-100 text-base leading-[150%] text-center">
-        Подходит, если вам нужно постоянно продавать одинаковый продукт
+        {{ text }}
       </p>
 
       <div class="flex flex-col items-center mt-12 max-md:mt-5 gap-[26px]">
         <p class="text-black font-semibold text-[32px] !leading-[150%]">
-          <span class="!font-normal"> от </span>
-          $300
+          <span class="!font-normal" v-if="price !== 'Договорная'"> от </span>
+          {{ price }}
         </p>
 
-        <ul class="flex flex-col gap-6">
+        <ul class="flex flex-col gap-6 px-1">
           <li
-            v-for="n of 4"
-            :key="n"
+            v-for="(item, i) of list"
+            :key="i"
             class="text-black text-center text-base !leading-[23px]"
           >
-            Чат и рассылка по клиентам, подключение мессенджером
+            {{ item }}
           </li>
         </ul>
       </div>
 
-      <Button class="px-8 py-4 max-w-[240px] w-full max-md:mt-10 mt-[112px]"
+      <Button class="px-8 py-4 max-w-[240px] w-full max-lg:mt-10 mt-[112px]"
         >Заказать</Button
       >
     </div>
@@ -38,6 +40,10 @@ import Button from '../Button.vue'
 
 defineProps<{
   popular?: boolean
+  list: string[]
+  title: string
+  text: string
+  price: string
 }>()
 </script>
 
--- src/components/Sections/Subscription.vue
+++ src/components/Sections/Subscription.vue
@@ -7,16 +7,18 @@
       <p
         class="max-w-[631px] w-full mx-auto text-center text-2xl max-md:text-base !leading-[150%]"
       >
-        Сколько проектов — столько и вариантов <br />
-        реализации. Выберите то, что подходит вашему <br />
-        продукту
+        Сколько проектов — столько и вариантов реализации. Выберите то, что
+        подходит вашему продукту
       </p>
       <div
-        class="flex gap-x-[25px] mt-6 items-center justify-center gap-6 max-lg:flex-wrap"
+        class="flex gap-x-[25px] mt-16 items-center justify-center gap-6 max-lg:flex-wrap"
       >
-        <SubscriptionCard />
-        <SubscriptionCard popular class="flex-[1.5px]" />
-        <SubscriptionCard />
+        <SubscriptionCard
+          v-for="(item, i) of subscriptions"
+          :popular="(i + 1) % 2 === 0"
+          :key="i"
+          v-bind="{ ...item }"
+        />
       </div>
     </div>
   </section>
@@ -24,6 +26,44 @@
 
 <script setup lang="ts">
 import SubscriptionCard from '@/components/Cards/SubscriptionCard.vue'
+
+const subscriptions = [
+  {
+    title: 'Вебинар',
+    text: 'Подходит, если вам нужно постоянно продавать одинаковый продукт',
+    list: [
+      'Платформа для вебинара',
+      'Чат и рассылка по клиентам',
+      'Автоворонка',
+      'Отправка записей и материаловна почту участникам',
+    ],
+    price: '$300',
+  },
+  {
+    title: 'Курс',
+    text: 'Подходит, если у вас есть курс из нескольких уроков с домашними заданиями и разными тарифами',
+    list: [
+      'Платформа для создания и ведения курса',
+      'Платформа для вебинаров',
+      'Чат и рассылка по клиентам, подключениемессенджером',
+      'Автоворонки и рекламные кампании',
+      'Автоворонки и рекламные кампании',
+      'Автоворонки и рекламные кампании',
+    ],
+    price: '$600',
+  },
+  {
+    title: 'Онлайн-школа',
+    text: 'Подходит, если вам нужно постоянно продавать одинаковый продукт',
+    list: [
+      'Платформа для вебинара',
+      'Чат и рассылка по клиентам',
+      'Автоворонка',
+      'Мгновенная прямая связь с поддержкой',
+    ],
+    price: 'Договорная',
+  },
+]
 </script>
 
 <style scoped></style>
